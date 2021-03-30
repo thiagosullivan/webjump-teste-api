34f60c47951fc7ce61256378f0a98e8bb9b4d667
Lucas Calazans <lucas.calazans@webjump.com.br>
2021-03-30 18:09:05 -0300

    Adding cors options

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var app = require('connect')()
 var serveStatic = require('serve-static')
+var cors = require('cors');
 
+app.use(cors())
 // Serve up mock-api folder
 app.use('/api', serveStatic('mock-api', {
   'index': false,
   'setHeaders': setJsonHeaders
 }))
- 
+
 // Set header to force download
 function setJsonHeaders (res, path) {
   res.setHeader('Content-type', 'application/json')
